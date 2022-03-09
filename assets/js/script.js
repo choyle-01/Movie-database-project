e43c7c8f86d5db1b1531908471b406fb3e978d83
Michael Hoyle <mcalebhoyle@gmail.com>
2022-03-09 14:05:37 -0500

    appending data to the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var searchForm = document.querySelector('#search-form')
 var inputTxt = document.querySelector('#inputText')
+var movieResults = document.querySelector('#movie-result')
 var array = JSON.parse(localStorage.getItem("History")) || [];
 
 var formSubmitHandler = function (event) {
@@ -10,7 +11,7 @@ var formSubmitHandler = function (event) {
   if (movieName) {
     getMovie(movieName);
     getGiphy(movieName);
-    populateHistory(movieName)
+    // populateHistory(movieName)
   }
 };
 
@@ -23,8 +24,39 @@ var getMovie = function (movie) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    console.log(data.Search);
+    movieResults.innerHTML = '';
+    for (let i = 0; i < data.Search.length; i++) {
+      var title = (data.Search[i].Title);
+      var titleResult = document.createElement('h5')
+      titleResult.textContent = title;
+      movieResults.append(titleResult);
+
+      var type = (data.Search[i].Type);
+      var typeResult = document.createElement('p');
+      typeResult.textContent = type;
+      movieResults.append(typeResult);
+
+      var releaseYear = (data.Search[i].Year);
+      var releaseYearResult = document.createElement('p');
+      releaseYearResult.textContent = releaseYear;
+      movieResults.append(releaseYearResult);
+
+      var imdbLink = (data.Search[i].imdbID)
+      var imdbLinkResult = document.createElement('a');
+      imdbLinkResult.setAttribute('target', '_blank')
+      imdbLinkResult.href = `https://www.imdb.com/title/${imdbLink}/`;
+      imdbLinkResult.textContent = `https://www.imdb.com/title/${imdbLink}/`;
+      movieResults.append(imdbLinkResult);
+
+      var poster = (data.Search[i].Poster);
+      var posterResults = document.createElement('img');
+      posterResults.setAttribute('alt', title + ' Poster')
+      posterResults.src= poster;
+      movieResults.append(posterResults);
+    }
   })
+
   .catch(function (err) {
     console.log(err);
   });
@@ -48,19 +80,19 @@ fetch(fetchURL)
 };
 
 // create function for history Log
-var populateHistory (movie){
-  Array.push(movieName);
-  localStorage.setItem("History", JSON.stringify(array));
-  renderHistory();
-};
+// var populateHistory = function (movieName) {
+//   Array.push(movieName);
+//   localStorage.setItem("History", JSON.stringify(array));
+//   renderHistory();
+//   for(let i = 0; i < array.length; i++){
+//     (function(){
+//       var searchLogEl = document.createElement('button');
+//       searchLogEl.textContent = array[i];
+//       searchLogEl.classList.add('btn btn-primary');
+//       searchLogEl.append(searchLogEl);
+//     })();
+//   }
+// };
 
-for(let i = 0; i < array.length; i++){
-  function(){
-    var searchLogEl = document.createElement('button');
-    searchLogEl.textContent = array[i];
-    searchLogEl.classList.add('btn btn-primary');
-    searchLogEl.append(searchLogEl);
-  };
-}
 
 searchForm.addEventListener('submit', formSubmitHandler);
